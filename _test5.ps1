0d082caf9fc38188ffb717069a5340728a3ba52e
ArakiW <ArakiW@users.noreply.github.com>
2026-08-17 22:48:10 +0800

    chore: remove leftover debug scripts

--- _test5.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-# switch-tools deps
-$env:PATH = "C:\msys64\usr\bin;$env:PATH"
-Write-Host ("T1: " + $env:PATH.Substring(0,40))
--- _test6.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-# switch-tools 的 exe 依赖 msys64 运行库
-$env:PATH = "C:\msys64\usr\bin;$env:PATH"
-Write-Host ("T2: " + $env:PATH.Substring(0,40))
